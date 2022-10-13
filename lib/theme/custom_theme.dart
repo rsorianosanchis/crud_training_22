6d1afa75bf06d30b141f828c3be21f2979910d2c
rsoriano <rsorianosanchis.developer@gmail.com>
2022-10-13 12:44:53 +0200

    working still  on productcard Pricedetail

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class CustomTheme {
 
   static ThemeData customTheme = ThemeData.light().copyWith(
+ 
+    
+
     scaffoldBackgroundColor: Colors.blueGrey[100],
     appBarTheme: const AppBarTheme(
       color: Colors.indigo,
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,3 +1,4 @@
+import 'package:crud_training_22/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
@@ -12,23 +13,143 @@ class ProductCard extends StatelessWidget {
         width: double.infinity,
         height: 400,
         decoration: _cardDecoration(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: const [
+            _BackGroundImage(),
+            _ProductDetails(),
+            Positioned(
+              child: _PriceDetail(),
+              top: 0,
+              right: 0,)
+          ]
+        ),
       ),
     );
   }
 
   BoxDecoration _cardDecoration() {
     return BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(25),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 10,
-            offset: Offset(0,2)
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 10,
+          offset: Offset(0,2)
+        )
+      ]
 
+    );
+  }
+}
+
+class _PriceDetail extends StatelessWidget {
+  const _PriceDetail({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      padding:const EdgeInsets.symmetric(horizontal: 20),
+      alignment: Alignment.center,
+      child: Text(
+        '200 \$',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize:20,
+          fontWeight: FontWeight.bold
+        ),
+        
+      ),
+      height: 70,
+      decoration: const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25),
+          topRight: Radius.circular(25))
+      ),
+
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(right: 70),
+      child: Container(
+        alignment: Alignment.center,
+        width:double.infinity,
+        height: 70,
+        
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        decoration: const BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25),
+            topRight: Radius.circular(25)
           )
-        ]
 
-      );
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Hard Disk G',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(height: 5,),
+            Text(
+              'Compatible with Windows Milenium and Monterey',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                fontSize: 15,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            )       
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BackGroundImage extends StatelessWidget {
+  const _BackGroundImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        child: const FadeInImage(
+          height: double.infinity,
+          width: double.infinity,
+          placeholder: AssetImage('assets/jar-loading.gif'), 
+          image:  NetworkImage('https://via.placeholder.com/400x300/d6d6d6.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
